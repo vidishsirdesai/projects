50d338a29da6a075146a6965214f10829c552505
Vidish Sirdesai <sirdesaividish+github@gmail.com>
2025-01-11 09:17:21 +0530

    flask app

--- src/onion_price_predictor.py
+++ src/onion_price_predictor.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("templates/index.html")
+    return render_template("index.html")
 
-# @app.route("/future_forecast", methods = ["GET"])
-# def future_forecast():
-#     pass
+@app.route("/forecast", methods = ["GET"])
+def forecast():
+    return render_template("forecast.html")
 
 
 if __name__ == "__main__":
