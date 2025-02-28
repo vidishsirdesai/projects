4f45f8ef503a8767ed90a8fa52caa9e52bc46a40
Vidish Sirdesai <sirdesaividish+github@gmail.com>
2025-02-28 11:35:15 +0530

    sensor data visualization

--- sensor_data_visualization/subject_1.py
+++ sensor_data_visualization/subject_1.py
@@ -8,15 +8,14 @@ sns.set_theme(style = "darkgrid")
 warnings.filterwarnings("ignore")
 
 # lift
-df_lift = pd.read_csv("dynamic_motion_classifier/data/subject_1/lift.csv")
-
+df_lift = pd.read_csv("data/subject_1/lift.csv")
 print("Action: Lift")
 print(df_lift["x"].max(), df_lift["x"].min())
 print(df_lift["y"].max(), df_lift["y"].min())
 print(df_lift["z"].max(), df_lift["z"].min())
 
 # roll_left
-df_roll_left = pd.read_csv("dynamic_motion_classifier/data/subject_1/roll_left.csv")
+df_roll_left = pd.read_csv("data/subject_1/roll_left.csv")
 
 print("Action: Roll Left")
 print(df_roll_left["x"].max(), df_roll_left["x"].min())
@@ -24,7 +23,7 @@ print(df_roll_left["y"].max(), df_roll_left["y"].min())
 print(df_roll_left["z"].max(), df_roll_left["z"].min())
 
 # roll_right
-df_roll_right = pd.read_csv("dynamic_motion_classifier/data/subject_1/roll_right.csv")
+df_roll_right = pd.read_csv("data/subject_1/roll_right.csv")
 
 print("Action: Roll Right")
 print(df_roll_right["x"].max(), df_roll_right["x"].min())
@@ -39,6 +38,9 @@ plt.title("Action: Lift")
 df_lift["x"].plot()
 df_lift["y"].plot()
 df_lift["z"].plot()
+plt.text(0.05, 0.95, f'x: max = {df_lift["x"].max()}, min = {df_lift["x"].min()}', transform = plt.gca().transAxes, verticalalignment = "top")
+plt.text(0.05, 0.90, f'y: max = {df_lift["y"].max()}, min = {df_lift["y"].min()}', transform = plt.gca().transAxes, verticalalignment = "top")
+plt.text(0.05, 0.85, f'z: max = {df_lift["z"].max()}, min = {df_lift["z"].min()}', transform = plt.gca().transAxes, verticalalignment = "top")
 plt.legend()
 
 plt.subplot(3, 1, 2)
@@ -46,6 +48,9 @@ plt.title("Action: Roll Left")
 df_roll_left["x"].plot()
 df_roll_left["y"].plot()
 df_roll_left["z"].plot()
+plt.text(0.05, 0.95, f'x: max = {df_roll_left["x"].max()}, min = {df_roll_left["x"].min()}', transform = plt.gca().transAxes, verticalalignment = "top")
+plt.text(0.05, 0.90, f'y: max = {df_roll_left["y"].max()}, min = {df_roll_left["y"].min()}', transform = plt.gca().transAxes, verticalalignment = "top")
+plt.text(0.05, 0.85, f'z: max = {df_roll_left["z"].max()}, min = {df_roll_left["z"].min()}', transform = plt.gca().transAxes, verticalalignment = "top")
 plt.legend()
 
 plt.subplot(3, 1, 3)
@@ -54,5 +59,8 @@ df_roll_right["x"].plot()
 df_roll_right["y"].plot()
 df_roll_right["z"].plot()
 plt.legend()
+plt.text(0.05, 0.95, f'x: max = {df_roll_right["x"].max()}, min = {df_roll_right["x"].min()}', transform = plt.gca().transAxes, verticalalignment = "top")
+plt.text(0.05, 0.90, f'y: max = {df_roll_right["y"].max()}, min = {df_roll_right["y"].min()}', transform = plt.gca().transAxes, verticalalignment = "top")
+plt.text(0.05, 0.85, f'z: max = {df_roll_right["z"].max()}, min = {df_roll_right["z"].min()}', transform = plt.gca().transAxes, verticalalignment = "top")
 
-plt.savefig("dynamic_motion_classifier/artifacts/sensor_data_visualization_subject_1.png")
+plt.savefig("artifacts/sensor_data_visualization_subject_1.png")
