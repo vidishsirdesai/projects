6ae3e9c52222eb0406a60cc32db0391fefdd1a8a
Vidish Sirdesai <sirdesaividish+github@gmail.com>
2025-03-02 10:30:55 +0530

    sensor data visualization

--- data_visualization/raw_data_viz.py
+++ data_visualization/raw_data_viz.py
@@ -93,8 +93,6 @@ def create_visualization(subject):
     plt.text(0.05, 0.85, f'z: max = {df_yaw_right["z"].max()}, min = {df_yaw_right["z"].min()}', transform = plt.gca().transAxes, verticalalignment = "top")
     plt.legend()
 
-    plt.tight_layout()
-
     # save plot
     plt.savefig(f"artifacts/raw_sensor_data_subject_{subject}.png")
 
